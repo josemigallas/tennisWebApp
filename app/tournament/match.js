4af73898a78d0c64706df6f29de6cee67fb78c07
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2015-11-06 18:44:49 +0100

    fixes module match and deletes class MatchError

--- app/tournament/match.js
+++ app/tournament/match.js
@@ -7,14 +7,8 @@ export class Match {
 
     addPlayer(player) {
         if (this.players.length >= 2) {
-            throw new MatchException("Match has already 2 players");
+            throw new Error("Match has already 2 players");
         }
         this.players.push(player);
-    }
-}
-
-export class MatchException extends Match {
-    constructor(msg) {
-        super(msg);
     }
 }
--- spec/app/tournament/match.spec.js
+++ spec/app/tournament/match.spec.js
@@ -1,5 +1,4 @@
 import * as match from "../../../app/tournament/match"
-import * as player from "../../../app/tournament/player"
 
 describe('class Match', function() {
     it('should be in 1 round by default', function() {
@@ -9,8 +8,8 @@ describe('class Match', function() {
 
     it('addPlayer should rise an error if more than 2 players are added', function() {
         var mMatch = new match.Match();
-        mMatch.addPlayer(new player.Player("Pepe", 1));
-        mMatch.addPlayer(new player.Player("Marta", 2));
-        expect( function() { mMatch.addPlayer(); }).toThrow(new match.MatchException("Match has already 2 players"));
+        mMatch.addPlayer();
+        mMatch.addPlayer();
+        expect( function() { mMatch.addPlayer(); }).toThrow(new Error("Match has already 2 players"));
     });
 });
