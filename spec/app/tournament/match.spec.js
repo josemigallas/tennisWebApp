aba1b834ef2781e6d087d2cd060532610c88f86f
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2015-11-07 18:50:19 +0100

    refactors match.spec for more readability

--- spec/app/tournament/match.spec.js
+++ spec/app/tournament/match.spec.js
@@ -3,6 +3,7 @@ import {ChildMatch} from "../../../app/tournament/match";
 import {Player} from "../../../app/tournament/player";
 
 describe('class Match', function() {
+
     it('should throw an error adding more than 2 players', function() {
         var match = new Match();
         match.addPlayer();
@@ -10,52 +11,59 @@ describe('class Match', function() {
 
         expect( function() { match.addPlayer(); }).toThrow(new Error("Match has already 2 players"));
     });
+
 });
 
 describe('class ChildMatch', function() {
 
-    it('should be in round 2 having both parents in round 1', function() {
-        var parentMatch1 = new Match(1);
-        var parentMatch2 = new Match(1);
-        var childMatch = new ChildMatch(parentMatch1, parentMatch2);
+    var spiderman = new Player("Spiderman", 1);
+    var batman = new Player("Batman", 3);
+    var theThing = new Player("The Thing", 5);
 
-        expect( childMatch.round ).toEqual(2);
+    var firstParent;
+    var secondParent;
+
+    beforeEach( function() {
+        firstParent = new Match(1);
+        secondParent = new Match(1);
+    });
+
+    it('should be in round 2 having both parents in round 1', function() {
+        var child = new ChildMatch(firstParent, secondParent);
+
+        expect( child.round ).toEqual(2);
     });
 
     it('should be in round 3 having first parent in round 1 and second in round 2', function() {
-        var parentMatch1 = new Match(1);
-        var parentMatch2 = new Match(2);
-        var childMatch = new ChildMatch(parentMatch1, parentMatch2);
+        secondParent.round = 2;
 
-        expect( childMatch.round ).toEqual(3);
+        var child = new ChildMatch(firstParent, secondParent);
+
+        expect( child.round ).toEqual(3);
     });
 
     it('should have automatically a winner having first parent only 1 player', function() {
-        var parentMatch1 = new Match(1);
-        parentMatch1.addPlayer(new Player("Spiderman", 1));
+        firstParent.addPlayer(spiderman);
 
-        var parentMatch2 = new Match(1);
-        parentMatch2.addPlayer(new Player("The Thing", 2));
-        parentMatch2.addPlayer(new Player("Batman", 3));
+        secondParent.addPlayer(theThing);
+        secondParent.addPlayer(batman);
 
-        var childMatch = new ChildMatch(parentMatch1, parentMatch2);
+        var child = new ChildMatch(firstParent, secondParent);
 
-        expect( childMatch.players.length ).toEqual(1);
-        expect( childMatch.players.pop().name ).toEqual("Spiderman");
+        expect( child.players.length ).toEqual(1);
+        expect( child.players.pop().name ).toEqual(spiderman.name);
     });
 
-    it('should throw an error trying to set a winner from unplayed match', function() {
-        var parentMatch1 = new Match(1);
-        parentMatch1.addPlayer(new Player("Spiderman", 1));
-        parentMatch1.addPlayer(new Player("The Thing", 2));
+    it('should throw an error trying to set a winner from an unplayed match', function() {
+        firstParent.addPlayer(spiderman);
+        firstParent.addPlayer(theThing);
 
-        var parentMatch2 = new Match(1);
-        parentMatch2.addPlayer(new Player("Batman", 3));
+        secondParent.addPlayer(batman);
 
-        var childMatch = new ChildMatch(parentMatch1, parentMatch2);
+        var child = new ChildMatch(firstParent, secondParent);
 
-        expect( childMatch.players.length ).toEqual(0);
-        expect( function() {childMatch.setWinnerFromParent(0,0)} ).not.toThrow(new Error("Parent match has not finished yet"));
-        expect( function() {childMatch.setWinnerFromParent(1,0)} ).toThrow(new Error("Parent match has not finished yet"));
+        expect( child.players.length ).toEqual(0);
+        expect( function() {child.setWinnerFromParent(0,0)} ).not.toThrow(new Error("Parent match has not finished yet"));
+        expect( function() {child.setWinnerFromParent(1,0)} ).toThrow(new Error("Parent match has not finished yet"));
     });
 });
