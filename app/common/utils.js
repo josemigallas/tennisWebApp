f741113b89c60a38c73b3a586117b55161dc7780
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2015-11-07 19:06:22 +0100

    removes unnecessary methods and test in module utils

--- app/common/utils.js
+++ app/common/utils.js
@@ -5,15 +5,3 @@ export function spliceItemRandomly(array) {
 export function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-
-export function isPowerOfTwo(num) {
-    return (num & (num - 1)) == 0;
-}
-
-export function findNextHigherPowerOfTwo(num) {
-    var nextPowerOfTwo = 1;
-    while (nextPowerOfTwo <= num) {
-        nextPowerOfTwo *= 2;
-    }
-    return nextPowerOfTwo;
-}
